eeaad150b5b206262de648f7ec75d24529e90389
Kevin Zhao <kevin8093@126.com>
2024-11-29 21:21:24 +0800

    update to graviton 4

--- dify-cdk/lib/EKS/instance-type-checker.ts
+++ dify-cdk/lib/EKS/instance-type-checker.ts
@@ -1,18 +1,18 @@
 import { EC2Client, DescribeInstanceTypeOfferingsCommand } from '@aws-sdk/client-ec2';
 
 // 初始化 EC2 客户端
-const ec2Client = new EC2Client({ region: 'us-west-2' });
+const ec2Client = new EC2Client({ region: 'ap-southeast-1' });
 
 // 检查实例类型的可用性
 export async function getAvailableInstanceType(): Promise<string> {
-  const instanceTypes = ['m7g.large', 'm6g.large', 'm6i.large'];
+  const instanceTypes = ['m8g.large, m7g.large', 'm6g.large', 'm6i.large'];
 
 
   for (const instanceType of instanceTypes) {
     const params = {
       Filters: [
         { Name: 'instance-type', Values: [instanceType] },
-        { Name: 'location', Values: ['us-west-2'] } // 确保使用正确的过滤条件
+        { Name: 'location', Values: ['ap-southeast-1'] } // 确保使用正确的过滤条件
       ]
     };
 
